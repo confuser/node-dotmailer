ac2c789dd313e32a0958480266669bed040979a7
Ian Crowther <i.crowther@fairfx.com>
2019-06-12 15:32:03 +0100

    adding a formData test

--- test/dotmailer.test.js
+++ test/dotmailer.test.js
@@ -2,6 +2,7 @@ const nock = require('nock')
 const assert = require('assert')
 const config = { auth: { user: '', pass: '' } }
 const dotMailer = require('../')(config)
+const fs = require('fs')
 
 describe('dotmailer', function () {
   it('should add a single piece of transactional data to a contact', function (done) {
@@ -30,6 +31,36 @@ describe('dotmailer', function () {
       config: {type: 'json', options: {}},
       cb: (error, res) => {
         assert.strictEqual(error.message, 'Unreplaced tokens')
+
+        done()
+      }
+    })
+  })
+
+  it('should import contacts using formData', function (done) {
+    // Mock the request
+    nock('https://api.dotmailer.com')
+      .post('/v2/address-books/1234/contacts/import')
+      .reply(202)
+
+    const options = {
+      file: {
+        value: fs.createReadStream(`${__dirname}/contacts.csv`),
+        options: {
+          filename: 'contacts.csv',
+          contentType: 'text/csv',
+          contentDisposition: 'contacts.csv'
+        }
+      }
+    }
+
+    dotMailer({
+      endpoint: 'PostAddressBookContactsImport',
+      tokens: [1234],
+      config: {type: 'formData', options},
+      cb: (error, res) => {
+        assert.strictEqual(error, undefined)
+        assert.strictEqual(res, undefined)
 
         done()
       }
